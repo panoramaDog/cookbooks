c88a44025bf4a16eac2fe99d55dbbebf5fcc48df
Adam F <68786858+afulton101222@users.noreply.github.com>
2024-03-13 00:30:52 -0400

    saving temp changes on new branch

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -1,49 +1,80 @@
-# Proxmox Full-Clone
-# ---
-# Create a new VM from a clone
+
+# resource "proxmox_lxc" "twingate" {    
+#     cores = 1
+#     cpuunits = 1024
+#     description = "macbook pw"
+#     #hostname = "twingate"
+#     memory = 512 # in MB
+#     network {
+#         name = "eth0"
+#         bridge = "vmbr0"
+#         firewall = true
+#         gw = "10.100.10.1"
+#         ip = "10.100.10.3/23"
+#     }
+#     onboot = true
+#     rootfs {
+#         size = "10G"
+#         storage = "local-lvm"
+#         #volume = "local-lvm:vm-102-disk-0"
+#     }
+#     swap = 512 # in MB
+#     #vmid = 102
+#     target_node = "pve"
+#     unprivileged = true
+# }   
+
+resource "proxmox_lxc" "unifi-controller" {
+    cores = 1
+    description = "macbook pw\n"
+    hostname = "unifi-controller"
+    memory = 1024
+    onboot = true
+    ostemplate = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+    password = ""
+    rootfs {
+        size = "8G"
+        storage = "local-lvm"
+        #volume = "local-lvm:vm-106-disk-0"
+    }
+    start = true
+    swap = 1024
+    target_node = "pve"
+    unprivileged = true
+}
 
 resource "proxmox_vm_qemu" "docker-host" {
-
-    # VM General Settings
-    target_node = "pve"
-    vmid = "120"
-    name = "docker-host"
-    desc = "Description"
-
-    # VM Advanced General Settings
-    onboot = true
-
-    # VM OS Settings
-    clone = "ubuntu-server-focal-docker"
-
-    # VM System Settings
     agent = 1
-
-    # VM CPU Settings
-    cores = 1
-    sockets = 1
-    cpu = "host"
-
-    # VM Memory Settings
-    memory = 1024
-
-    # VM Network Settings
+    ciuser = "root"
+    cpu = "kvm64"
+    cores = 4
+    desc = "Ubuntu Server Focal Image with Docker pre-installed\n\n## Disk utilization command\n`gdu --ignore-dirs-pattern /mnt /`\n\n## Mount Media Share\n`mount -t cifs -o username=media,password=unraveled-deceptive-livable, //10.100.10.15/media //mnt/media`"
+    full_clone = false
+    memory = 12288 # in MB
+    name = "docker-host"
+    ipconfig0 = "ip=10.100.10.20/24,gw=10.100.10.1"
     network {
         bridge = "vmbr0"
         model  = "virtio"
     }
+    onboot = true
+    qemu_os = "other"
+    sockets = 1
+    target_node = "pve"
+    disks {
+      virtio {
+        virtio0 {
+            disk{
+                size = 200 # in GB
+                storage = "storageSSD"
+                replicate = true
+                }
+            }
+        }
+    }
+    #vmid = "103"
 
     # VM Cloud-Init Settings
     os_type = "cloud-init"
-
-    # (Optional) IP Address and Gateway
-    ipconfig0 = "ip=10.100.10.59/24,gw=10.100.10.1"
-
-    # (Optional) Default User
-    # ciuser = "your-username"
-
-    # (Optional) Add your SSH KEY
-    # sshkeys = <<EOF
-    # #YOUR-PUBLIC-SSH-KEY
-    # EOF
+    scsihw = "virtio-scsi-pci"
 }
--- terraform/readme.md
+++ terraform/readme.md
@@ -1,13 +1,27 @@
 # Terraform
 
+the documentation for the Proxmox provider can be found [here](https://registry.terraform.io/providers/Telmate/proxmox/latest/docs)
+
 ## Overview
 
 In the terraform directory a credentials file needs to be created: `credentials.tf`
 
 Contents of credentials file:
 
-```
+```terraform
 do_api_token = "api_token_here"
 ```
 
 In order to run the terraform, in the folder first issue the command `terraform init`. Next, you can use different `terraform` flags such as `terraform plan/validate/apply/destroy`.
+
+## Importing Proxmox Resources
+
+```bash
+# import VM into terraform
+# terraform import {{<resource mapping in .tf file>}} {{<node>/<type>/<vmid>}}
+# remove brackets, those are placeholders for to show bounds of each variable
+terraform import proxmox_vm_qemu.docker-host pve/vm/103
+
+# import container into terraform
+terraform import proxmox_lxc.twingate pve/lxc/102
+```
